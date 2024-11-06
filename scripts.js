2e5b396a3c209f3551f16e598b4aee3dbeaea5c3
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-06 12:22:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -92,5 +92,50 @@ const input = document.querySelector("input")
 
 // O evento de input é disparado quando você digita algo no mesmo
 input.addEventListener("input", ()=>{
-    console.log(input.value)
+    const value = input.value
+
+    const regex = /\D+/g
+
+    //retorna o padrão encontrado na string
+    /* console.log(value.match(regex)) */
+
+    // Testa se atende o padrão
+    /* const isValid = regex.test(value)
+    console.log(isValid) */
 }) 
+
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    const value = input.value
+
+    const regex = /\D+/g //testa e valida expressões regulares
+
+    if(regex.test(value)){
+        console.log(value)
+    }else{
+        alert("Valor inválido, digite corretamente") 
+    }
+
+    /* const value = input.value.replace(regex,"")
+    console.log(value) */
+})
+
+const input1 = document.querySelector("input")
+const form1 = document.querySelector("form")
+
+form1.onsubmit = (event)=>{
+    event.preventDefault()
+
+    const value = input.value
+    const hasNumberRegex = /\d+/g
+
+    if(hasNumberRegex.text(value)){
+        alert("O texto contém números, por favor, digite corretamente")
+    }else{
+        alert("Cadastro enviado!")
+    }
+
+}
